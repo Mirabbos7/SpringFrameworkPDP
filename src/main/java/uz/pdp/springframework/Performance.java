aff1d4dd06e23d9a86832dadb19359d1f4a76572
Mirabbos7 <mirabbosahmedov7777@gmail.com>
2025-02-14 20:35:59 +0500

    Not working XD

--- src/main/java/uz/pdp/springframework/Performance.java
+++ src/main/java/uz/pdp/springframework/Performance.java
@@ -4,18 +4,12 @@ import java.util.Random;
 
 public class Performance {
     public void perform(){
-        // turn off phones
-        // take a seat
         Random random = new Random();
 
         if (random.nextBoolean()) {
             System.out.println("performing well..");
         }else
             throw new RuntimeException("Performed Bad");
-        /*
-         *  clap clap /// applause
-         *  complaining about a action
-         * */
     }
 
 }
